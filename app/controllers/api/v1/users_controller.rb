afc23cf71907a4aa47578f5c85b8044cd1bb2789
roman9891 <roman9891@gmail.com>
2021-01-27 08:52:56 -0500

    added id to serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,26 @@
 class Api::V1::UsersController < ApplicationController
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+  skip_before_action :authorized, only: [:create]
+ 
+  def create
+    @user = User.create(user_params)
+    puts @user.id
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token, id:@user.id }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
-     
-    private
-    def user_params
-        params.require(:user).permit(:username, :password, :bio, :avatar)
-    end
+  end
+
+  def update 
+    @user = User.find_by(id:params[:id])
+    @user.update(user_params)
+    render json: { user: UserSerializer.new(@user), jwt: @token }, status: :accepted
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :progress)
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :avatar, :bio
+  attributes :id, :username, :progress
 end
